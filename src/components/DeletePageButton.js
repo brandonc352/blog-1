27f031aad36e5d7c8996999052010332a3c9614d
“drew” <brandonc503@gmail.com>
2023-01-26 21:32:13 -0500

    Missed files: Commiting EditPage.js and DeletePageButton.js

---
+++ src/components/DeletePageButton.js
@@ -0,0 +1,24 @@
+
+import { useParams } from 'react-router-dom'
+
+const DeleteContent = () => {
+    let { id } = useParams()
+
+    const handleDelete = (event) => {
+        event.preventDefault()
+        var answer = window.confirm("Delete content?");
+        if (answer) {
+            fetch(`http://localhost:7500/pages/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+        }
+    }
+  
+    return (
+        <a href="/delete" className="deleteBTN" onClick={handleDelete}>Delete Page</a>
+    )
+}
+export default DeleteContent
---
+++ src/components/pages/EditPage.js
@@ -0,0 +1,71 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react"
+
+export default function EditPage() {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    const [titleValue, setTitleValue] = useState()
+    const [bodyValue, setBodyValue] = useState()
+
+    const handleTitleChange = (event) => {
+        setTitleValue(event.target.value)
+    }
+    const handleBodyChange = (event) => {
+        setBodyValue(event.target.value)
+    }
+    let { id } = useParams()
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        fetch(`http://localhost:7500/pages/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: titleValue,
+                body: bodyValue
+            })
+        })
+    }
+    useEffect(() => {
+        fetch(`http://localhost:7500/pages/${id}`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw response
+            })
+            .then((data) => {
+                setData(data)
+
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error)
+                setError(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) return ""
+    if (error) return "Error!"
+
+    return (
+        <>
+            <h1>Edit Page</h1>
+            {titleValue ? "" : setTitleValue(data.title)}
+            {bodyValue ? "" : setBodyValue(data.body)}
+            <form onSubmit={handleSubmit}>
+                <label >Title</label><br />
+                <input id="title" type="text" value={titleValue ? titleValue : data.title} onChange={handleTitleChange} />
+                <br /><br /><label >Body</label><br />
+                <input id="body" type="text" value={bodyValue ? bodyValue : data.body} onChange={handleBodyChange} /><br /><br />
+
+                <button className="saveBTN">Save</button>
+            </form>
+        </>
+    )
+}
